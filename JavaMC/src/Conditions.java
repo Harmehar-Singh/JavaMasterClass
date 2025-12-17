6cd83644e311ad5805b1e1ab061d5a16bdd62f1d
Harmehar-Singh <harmehar.singh@outlook.com>
2025-12-17 17:51:38 -0500

    Conditions

---
+++ JavaMC/src/Conditions.java
@@ -0,0 +1,12 @@
+public class Conditions {
+    public static void main(String[] args) {
+        System.out.println("Hello, Harmehar");
+        boolean isAlien = false;
+        if (isAlien == false){
+            System.out.println("You are not an alien!");
+        }
+        else{
+            System.out.println("You are an alien!");
+        }
+    }
+}
--- JavaMC/src/FirstClass.java
+++ JavaMC/src/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
-    static void main(String[] args) {
+
+    public static void main(String[] args) {
         System.out.println("Hello World");
+
     }
 }
